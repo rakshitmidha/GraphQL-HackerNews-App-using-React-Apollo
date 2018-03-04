49ae59afcc45053cf4973df6495eecea3bd6545e
rakshitmidha <rmidha@collaboration-factory.de>
2018-03-04 16:35:41 +0100

    Sending feedQuery to apollo and getting data

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
 import Link from './Link'
 
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
 class LinkList extends Component {
     render() {
-        const linkdsToRender = [
-            {
-                id: '1',
-                description: 'Prisma turn db into GraphQL api',
-                url: 'rakshitmidha.cf',
-            },
-            {
-                id: '2',
-                description: 'Prisma is a GraphQL client',
-                url: 'https://www.apollographql.com/docs/react/',
-            },
-        ]
+        // 1
+        if (this.props.feedQuery && this.props.feedQuery.loading) {
+            return <div>Loading</div>
+        }
+
+        // 2
+        if (this.props.feedQuery && this.props.feedQuery.error) {
+            return <div>Error</div>
+        }
+
+        // 3
+        const linksToRender = this.props.feedQuery.feed.links
 
         return (
-            <div>
-                {
-                    linkdsToRender.map(link => <Link key={link.id} link={link} />)
-                }
-            </div>
+            <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
         )
     }
 }
 
-export default LinkList
+
+// 1
+const FEED_QUERY = gql`
+  # 2
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+// 3
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList)
