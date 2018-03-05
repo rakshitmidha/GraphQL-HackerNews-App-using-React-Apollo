c40e30ae90ab35b991e72cd0a722830a4c4cdb7e
rakshitmidha <rmidha@collaboration-factory.de>
2018-03-05 21:03:41 +0100

    Running prisma server on docker

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/public-gentlesentry-488/hackernews-graphql-js/dev',
+      endpoint: 'http://localhost:4466/hackernews-graphql-js/dev',
       secret: 'mysecret123',
       debug: true
     }),
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = new HttpLink(
     {
-        uri: 'http://localhost:3000'
+        uri: 'http://localhost:4000'
     }
 )
 
