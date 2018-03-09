f798eb10bebbf0f022a5f5dd7b354f1e9df38343
rakshitmidha <rmidha@collaboration-factory.de>
2018-03-09 22:38:44 +0100

    Adding header, create new & result component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
-//import LinkList from './LinkList'
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import LinkList from './LinkList';
 import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
     return (
-      <CreateLink />
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
