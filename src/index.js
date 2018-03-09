e04eed93fa2f71fd7a86c7ef2b380c13b65b3406
rakshitmidha <rmidha@collaboration-factory.de>
2018-03-09 22:37:13 +0100

    Using browser router

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient} from 'apollo-client';
+import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
 
 const httpLink = new HttpLink(
     {
@@ -23,8 +24,10 @@ const client = new ApolloClient(
 )
 
 ReactDOM.render(
-    <ApolloProvider client={client}
-        ><App />
-    </ApolloProvider>
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>
     , document.getElementById('root'));
 registerServiceWorker();
