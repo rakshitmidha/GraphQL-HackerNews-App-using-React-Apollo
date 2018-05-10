0e6c0c0d64c954aaf31caf3c40f7802dd207d100
rakshitmidha <rmidha@collaboration-factory.de>
2018-05-10 20:50:33 +0200

    Add new routes for top & new page

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 
 import Header from './Header';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink'
 import Login from './Login'
@@ -16,10 +16,12 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/" render={() => <Redirect to='/new/1/' />} />
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
             <Route exact path='/search' component={Search} />
+            <Route exact path='/top' component={LinkList} />
+            <Route exact path='/new/:page' component={LinkList} />
           </Switch>
         </div>
       </div>
