491b8789253c3d94b3f793c59586b34b639e07c0
rakshitmidha <rmidha@collaboration-factory.de>
2018-05-10 21:06:51 +0200

    Manipulating graphql query in LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -148,4 +148,16 @@ export const FEED_QUERY = gql`
 `
 
 // 3
-export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
+export default graphql(FEED_QUERY, {
+    name: 'feedQuery',
+    options: ownProps => {
+        const page = parseInt(ownProps.match.params.page, 10)
+        const isNewPage = ownProps.location.pathname.includes('new')
+        const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+        const first = isNewPage ? LINKS_PER_PAGE : 100
+        const orderBy = isNewPage ? 'createdAt_DESC' : null
+        return {
+            variables: { first, skip, orderBy },
+        }
+    },
+})(LinkList)
